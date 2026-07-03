48f3052c2f6b96da0ee433831afa669115083c75
bernadette0731 <tariaobernadette@gmail.com>
2026-07-03 18:54:31 +0800

    Modified files

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Grade System</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet"
 </head>
 <body>
     <div class="landing-container">
